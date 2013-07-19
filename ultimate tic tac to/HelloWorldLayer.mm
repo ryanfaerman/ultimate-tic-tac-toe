086b2448b873aef54e7613e9027c714e60d9e5b4
Ryan Faerman <ry@nwitty.com>
2013-07-18 22:15:53 -0400

    week 2

--- ultimate tic tac to/HelloWorldLayer.mm
+++ ultimate tic tac to/HelloWorldLayer.mm
@@ -25,7 +25,15 @@ enum {
 
 CCSprite *player_x;
 CCSprite *player_o;
-CCSprite *double_o;
+CCSprite *board;
+
+CCSprite *o_source;
+CCSprite *x_source;
+
+CCSprite *moving_target;
+
+BOOL is_x_turn = YES;
+BOOL first_contact = YES;
 
 #pragma mark - HelloWorldLayer
 
@@ -54,17 +62,6 @@ CCSprite *double_o;
 {
 	if( (self=[super init])) {
 		
-		// enable events
-    
-    b2Vec2 gravity = b2Vec2(0.0f, 0.0f);
-    _world = new b2World(gravity);
-    
-    m_debugDraw = new GLESDebugDraw( PTM_RATIO );
-    _world->SetDebugDraw(m_debugDraw);
-    
-    uint32 flags = 0;
-    flags += b2Draw::e_shapeBit;
-    m_debugDraw->SetFlags(flags);
 		
 		self.touchEnabled = YES;
 		self.accelerometerEnabled = YES;
@@ -79,107 +76,70 @@ CCSprite *double_o;
     [bg setPosition:ccp(s.width * 0.5f, s.height * 0.5f)];
     [self addChild:bg];
 		
+    board = [CCSprite spriteWithFile:@"board.png"];
+    board.position = ccp(520, 385);
+    [self addChild:board];
+    
     //Set up sprites
-		player_x = [CCSprite spriteWithFile:@"11-x@2x.png"];
-    player_x.position = ccp(50, 100);
-    [self addChild:player_x];
-    [self addBoxBodyForSprite:player_x];
+//		player_x = [CCSprite spriteWithFile:@"11-x@2x.png"];
+//    player_x.position = ccp(50, 100);
+//    [self addChild:player_x];
     
-    player_o = [CCSprite spriteWithFile:@"12-o@2x.png"];
-    player_o.position = ccp(200, 300);
-    [self addChild:player_o];
+    moving_target = [CCSprite spriteWithFile:@"32-circle-o@2x.png"];
+    moving_target.position = ccp(950, 300);
+    [self addChild:moving_target];
     
-    double_o = [CCSprite spriteWithFile:@"32-circle-o@2x.png"];
-    double_o.position = ccp(200, 400);
-    [self addChild:double_o];
-    [self addBoxBodyForSprite:double_o];
+    o_source = [CCSprite spriteWithFile:@"12-o@2x.png"];
+    o_source.position = ccp(70, 350);
+    [self addChild:o_source];
+    x_source = [CCSprite spriteWithFile:@"11-x@2x.png"];
+    x_source.position = ccp(70, 450);
+    [self addChild:x_source];
+    
     
     [self schedule:@selector(nextFrame:)];
+    
+    
 		
 		
 		
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Tap screen" fontName:@"Marker Felt" fontSize:32];
-		[self addChild:label z:0];
-		[label setColor:ccc3(0,0,255)];
-		label.position = ccp( s.width/2, s.height-50);
+//		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Tap screen" fontName:@"Marker Felt" fontSize:32];
+//		[self addChild:label z:0];
+//		[label setColor:ccc3(0,0,255)];
+//		label.position = ccp( s.width/2, s.height-50);
     
     [[SimpleAudioEngine sharedEngine] playBackgroundMusic:@"Fuelship_Syphus.mp3"];
-    [CDAudioManager sharedManager].backgroundMusic.volume = 0.3f;
+    [CDAudioManager sharedManager].backgroundMusic.volume = 0.2f;
 		
-    [self schedule:@selector(tick:)];
+//    [self schedule:@selector(tick:)];
 		[self scheduleUpdate];
 	}
 	return self;
 }
 
-- (void)addBoxBodyForSprite:(CCSprite *)sprite {
-  
-  b2BodyDef spriteBodyDef;
-  spriteBodyDef.type = b2_dynamicBody;
-  spriteBodyDef.position.Set(sprite.position.x/PTM_RATIO,
-                             sprite.position.y/PTM_RATIO);
-  spriteBodyDef.userData = sprite;
-  b2Body *spriteBody = _world->CreateBody(&spriteBodyDef);
-  
-  b2PolygonShape spriteShape;
-  spriteShape.SetAsBox(sprite.contentSize.width/PTM_RATIO/2,
-                       sprite.contentSize.height/PTM_RATIO/2);
-  b2FixtureDef spriteShapeDef;
-  spriteShapeDef.shape = &spriteShape;
-  spriteShapeDef.density = 10.0;
-  spriteShapeDef.isSensor = false;
-  spriteBody->CreateFixture(&spriteShapeDef);
-  
-}
-
--(void) draw
-{
-	//
-	// IMPORTANT:
-	// This is only for debug purposes
-	// It is recommend to disable it
-	//
-	[super draw];
-	
-	ccGLEnableVertexAttribs( kCCVertexAttribFlag_Position );
-	
-	kmGLPushMatrix();
-	
-	_world->DrawDebugData();
-	
-	kmGLPopMatrix();
-}
-
-- (void)tick:(ccTime)dt {
-  
-  _world->Step(dt, 10, 10);
-  for(b2Body *b = _world->GetBodyList(); b; b=b->GetNext()) {
-    if (b->GetUserData() != NULL) {
-      CCSprite *sprite = (CCSprite *)b->GetUserData();
-      
-      b2Vec2 b2Position = b2Vec2(sprite.position.x/PTM_RATIO,
-                                 sprite.position.y/PTM_RATIO);
-      float32 b2Angle = -1 * CC_DEGREES_TO_RADIANS(sprite.rotation);
-      
-      b->SetTransform(b2Position, b2Angle);
-    }
-  }
-  
-}
-
 -(void) nextFrame:(ccTime)dt
 {
-  player_o.position = ccp(player_o.position.x + 100*dt, player_o.position.y);
+  moving_target.position = ccp(moving_target.position.x + 100*dt, moving_target.position.y);
   
-  CGRect movingRect = [player_o boundingBox];
+  CGRect movingRect = [moving_target boundingBox];
   CGRect player = [player_x boundingBox];
   if(CGRectIntersectsRect(movingRect, player)) {
-    NSLog(@"ha ha Collision detected");
-    [[SimpleAudioEngine sharedEngine] playEffect:@"garble.mp3"];
+    
+    if (first_contact == YES) {
+      NSLog(@"ha ha Collision detected");
+      [[SimpleAudioEngine sharedEngine] playEffect:@"garble.mp3"];
+      first_contact = NO;
+    }
+    
+//    
+    
+  } else {
+    first_contact = YES;
   }
   
-  if (player_o.position.x > [[CCDirector sharedDirector] winSize].width + 32) {
-    player_o.position = ccp(-32, player_o.position.y);
+  
+  if (moving_target.position.x > [[CCDirector sharedDirector] winSize].width + 32) {
+    moving_target.position = ccp(-32, moving_target.position.y);
   }
   
 }
@@ -197,42 +157,40 @@ CCSprite *double_o;
 - (void)ccTouchEnded:(UITouch *)touch withEvent:(UIEvent *)event {
 	CGPoint location = [self convertTouchToNodeSpace: touch];
   
-	[player_x stopAllActions];
-  id move = [CCMoveTo actionWithDuration:0.2f position:location];
+  CGRect boardRect = [board boundingBox];
+  CGRect pointRect = CGRectMake(location.x, location.y, 1.0f, 1.0f);
+  if(!CGRectIntersectsRect(boardRect, pointRect)) {
+    return;
+  }
+    
+  CCSprite *player;
+  
+  if (is_x_turn) {
+    player = [CCSprite spriteWithFile:@"11-x@2x.png"];
+    player.position = ccp(70, 450);
+    player_x = player;
+  } else {
+    player = [CCSprite spriteWithFile:@"12-o@2x.png"];
+    player.position = ccp(70, 350);
+  }
+  
+  is_x_turn = !is_x_turn;
+  [self addChild:player];
+  
+  id move = [CCMoveTo actionWithDuration:0.3f position:location];
   id wrapperAction = [CCCallFunc actionWithTarget:self selector:@selector(actionComplete)];
-	[player_x runAction: [CCSequence actions:move, wrapperAction, nil]];
+  [player runAction: [CCSequence actions:move, wrapperAction, nil]];
+  
   
 }
 
 -(void) actionComplete
 {
   
-  CGRect targetRect = [double_o boundingBox];
-  CGRect player = [player_x boundingBox];
-  if(CGRectIntersectsRect(targetRect, player)) {
-    NSLog(@"ha ha Collision detected");
-    [[SimpleAudioEngine sharedEngine] playEffect:@"garble.mp3"];
-  } else  {
-    [[SimpleAudioEngine sharedEngine] playEffect:@"thid.mp3"];
-  }
+  [[SimpleAudioEngine sharedEngine] playEffect:@"thid.mp3"];
+
   
 }
-
--(void) dealloc
-{
-	delete _world;
-	_world = NULL;
-	
-	delete m_debugDraw;
-	m_debugDraw = NULL;
-	
-	[super dealloc];
-}	
-
-
-
-
-
 
 
 #pragma mark GameKit delegate
