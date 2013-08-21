25311d2f1405c3e6a665d120722c0c3043e9c771
Ryan Faerman <ry@nwitty.com>
2013-08-20 23:11:35 -0400

    Define common protocol for all leader board engines

--- ultimate tic tac to/RWLeaderboardProtocol.h
+++ ultimate tic tac to/RWLeaderboardProtocol.h
@@ -7,7 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RWScore.h"
 
 @protocol RWLeaderboardProtocol <NSObject>
+
+- (void) push:(RWScore *)score;
+
+// get the top `quantity` of scores for the past `days` number of days
+- (NSArray *) getTop:(NSNumber *)quantity forLast:(NSNumber *)days;
+
+// uses getTop:forLast: with a 7 day default
+- (NSArray *) getTop:(NSNumber *)quantity;
+
+- (BOOL) available;
 
 @end
