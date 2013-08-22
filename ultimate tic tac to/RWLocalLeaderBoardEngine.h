96d20be24b68d5fbce06022de004fecb15825713
Ryan Faerman <ry@nwitty.com>
2013-08-21 23:13:00 -0400

    Implement a storage engine for leaderboards
    This basic engine provides local storage to a Sqlite3 database of a
    leaderboard. It implements the LeaderBoardProtocol that the LeaderBoard
    requires.
    
    It handles all the database creation, queries, and maintenence.

---
+++ ultimate tic tac to/RWLocalLeaderBoardEngine.h
@@ -0,0 +1,13 @@
+//
+//  RWLocalLeaderBoardEngine.h
+//  ultimate tic tac to
+//
+//  Created by Ryan Faerman on 8/21/13.
+//  Copyright (c) 2013 Ryan Faerman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWLocalLeaderBoardEngine : NSObject
+
+@end
---
+++ ultimate tic tac to/RWLocalLeaderBoardEngine.m
@@ -0,0 +1,13 @@
+//
+//  RWLocalLeaderBoardEngine.m
+//  ultimate tic tac to
+//
+//  Created by Ryan Faerman on 8/21/13.
+//  Copyright (c) 2013 Ryan Faerman. All rights reserved.
+//
+
+#import "RWLocalLeaderBoardEngine.h"
+
+@implementation RWLocalLeaderBoardEngine
+
+@end
